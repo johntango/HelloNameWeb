8b7bcad67ec6f97f144bc608441bad862b7ae1db
John Williams <jrw+3@mit.edu>
2024-02-10 12:50:33 +0000

    ejs added

--- server.js
+++ server.js
@@ -1,62 +1,41 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let path = require('path');
+const ejs = require('ejs'); // Import the ejs module
 let app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
-const port = 3000;
+//let ejs = require('ejs');
+const port = 3001;
 
+// serve from public
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
 
-
-let names = []; // Array to store names
+let names = ['Fred']; // Array to store names
 
 app.get('/', (req, res) => {
-    let greeting = '';
-    if (names.length > 0) {
-      greeting = `<h1>Hello ${names[names.length - 1]}</h1>`;
-    }
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-          <title>Enter Name</title>
-      </head>
-      <body>
-          ${greeting}
-          <form action="/submit-name" method="POST">
-              <input type="text" name="username" placeholder="Enter your name" required>
-              <button type="submit">Submit</button>
-          </form>
-      </body>
-      </html>
-    `);
-  });
+  let namelist = "";
+  if (names.length > 0) {
+    namelist = '<ul>';
+    names.map((name) => {
+      namelist += `<li>${name}</li>`;
+    });
+    namelist += '</ul>';
+  }
 
-app.post('/submit-name', (req, res) => {
-    const name = req.body.username;
+  // Render the index.ejs template and pass the namelist as a variable
+  ejs.renderFile( path.join(__dirname, './views', 'index.ejs'), { namelist: namelist });
+});
+
+app.post('/add-name', (req, res) => {
+    const name = req.body.name;
     names.push(name);
     res.redirect('/');
   });
 // New route to list all names
 app.get('/names', (req, res) => {
-    let namesList = '<ul>';
-    for (let name of names) {
-      namesList += `<li>${name}</li>`;
-    }
-    namesList += '</ul>';
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-          <title>Name List</title>
-      </head>
-      <body>
-          <h1>All Names</h1>
-          ${namesList}
-          <p><a href="/">Back</a></p>
-      </body>
-      </html>
-    `);
+    res.json(names);
   });
   
 app.listen(port, () => {
